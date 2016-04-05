0255ee48d86b956ef97c1c0e4ce165f2e3427e99
vincebarresi <vincebarresi@gmail.com>
2016-04-04 21:37:54 -0400

    update animations

--- Animations/Animations/ViewController.swift
+++ Animations/Animations/ViewController.swift
@@ -11,27 +11,60 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 1
+    var timer = NSTimer()
+    var isAnimating = true
     
-    @IBAction func updateImageBtn(sender: AnyObject) {
+    @IBOutlet weak var gifMeBtn: UIButton!
+    @IBAction func gifMeBtn(sender: AnyObject) {
+        musicImage.image = UIImage(named: "music-frame1.png")
+       
         
-        count == 5 ? [count = 1] : [count += 1]
-        
-        musicImage.image = UIImage(named: "music-frame\(count).png")
-        
+        if isAnimating == true  {
+            timer.invalidate()
+            isAnimating = false
+            gifMeBtn.setTitle(".gif me", forState: UIControlState.Normal)
+            
+        } else {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            isAnimating = true
+            gifMeBtn.setTitle("un .gif me", forState: UIControlState.Normal)
+        }
+
     }
+    
     
     @IBOutlet weak var musicImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
+        
+    }
+    
+    func doAnimation() {
+        count == 5 ? [count = 1] : [count += 1]
+        
+        musicImage.image = UIImage(named: "music-frame\(count).png")
     }
 
-
+    /*
+    override func viewDidLayoutSubviews() {
+        musicImage.center = CGPointMake(musicImage.center.x - 400, musicImage.center.y)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(1) { () -> Void in
+            
+            self.musicImage.center = CGPointMake(self.musicImage.center.x+400, self.musicImage.center.y)
+        }
+        
+    }
+*/
 }
 
