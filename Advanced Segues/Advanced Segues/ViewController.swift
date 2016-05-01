eb96878f7f1529d3b8a43f368d523b27e14220ad
vincebarresi <vincebarresi@gmail.com>
2016-04-30 20:35:40 -0400

    add advanced segues

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+//  Created by Vincent Barresi on 4/30/16.
+//  Copyright © 2016 Vincent Barresi. All rights reserved.
+//
+
+import UIKit
+
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        print(rowCounter)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
